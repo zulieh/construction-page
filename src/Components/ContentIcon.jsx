cd48f88317b419d8303fcc3ef87ad9a43cf973c2
zulieh <tzuleiha@gmail.com>
2024-08-11 12:28:35 +0100

    hero1

---
+++ src/Components/ContentIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ContentIcon({children, title, text}) {
+  return (
+    <div className= ''>
+        <div className='flex gap-4 hover:text-orange-600'>
+        <div className="flex justify-center items-center h-20 w-20 rounded-full bg-orange-600 hover:animate-bounce">
+           <icon className='m-2 text-5xl text-orange-900'>{children}</icon>
+        </div>
+         <h2 className='text-3xl w-3 pb-6 ml-5'>{title}</h2>
+         </div>
+         <p className='ml-28'>{text}</p>
+      </div>
+  )
+}
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
 import { CarouselWithContent } from './carousel';
-// import lightImage from './assets/images/light.jpg';
+import ContentIcon from './ContentIcon';
+import { IoMdSettings } from "react-icons/io";
+import { FaProjectDiagram } from "react-icons/fa";
+import { GrProjects } from "react-icons/gr";
+import { IoConstructSharp } from "react-icons/io5";
+import { SiConstruct3 } from "react-icons/si";
+import { LuConstruction } from "react-icons/lu";
+
+
 
 const Landing = () => {
   return (
     <div>
-        <div className=" translate-x-6 ease-in duration-300">
-            <CarouselWithContent />
-        </div>  
+        <CarouselWithContent />
+        <div className='px-28 '>
+            <h1 className='text-5xl mb-10'>OUR SERVICES</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+                <div>
+                    <ContentIcon 
+                        children= {<IoMdSettings />}
+                        title="General Construction"
+                        text="We have engineers and architects suitable to work with on 
+                        the majority of items in an entire project, handling all types of projects."
+                    />
+                </div>
+                <div>
+                    <ContentIcon 
+                        children= {<FaProjectDiagram />}
+                        title="Renovation projects"
+                        text="Some of the works have been weather-damaged, tarnished,or outdated for a long time
+                        We provide refurbishment, upgrading, make it like new."
+                    />
+                </div>
+                <div>
+                    <ContentIcon 
+                        children= {<GrProjects />}
+                        title="Interior design"
+                        text="Our architects will help your interiors look like an artwork. 
+                        Along with it is the interior layout, furnishings are also consulted and installed by."
+                    />
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 pt-12'>
+                <div>
+                    <ContentIcon 
+                        children= {<IoConstructSharp />}
+                        title="Consultation projects"
+                        text="Have you had trouble with the classic electrical system? 
+                        Are you looking for a custom power solution? Our electrical engineers will help you!"
+                    />
+                </div>
+                <div>
+                    <ContentIcon 
+                        children= {<SiConstruct3 />}
+                        title="Renovation projects"
+                        text="Our experts have many years of technical and construction experience, 
+                        and they will advise on the daily challenges that contractors to face."
+                    />
+                </div>
+                <div>
+                    <ContentIcon 
+                        children= {<LuConstruction />}
+                        title="Projects Partnering"
+                        text="We co-operate with many reputable contractors, undertake many large and small projects, 
+                        covering all aspects of electricity, irrigation, bridges…etc."
+                    />
+                </div>
+            </div>
+        </div>
     </div>
-  )
+  );
 }
 
 export default Landing;
