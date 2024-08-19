f29ef50776f51d7db20eb76fe89ddeb13cb39e9e
zulieh <tzuleiha@gmail.com>
2024-08-19 23:40:36 +0100

    projects

---
+++ src/Components/Pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroBanner from '../HeroBanner'
+
+function Projects() {
+  return (
+    <div>
+        <HeroBanner 
+        text="Projects"/>
+        
+    </div>
+  )
+}
+
+export default Projects
